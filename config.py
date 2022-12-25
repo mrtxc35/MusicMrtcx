57fe102e4000e501c880adcebb118a8a6abbc365
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-25 17:35:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_USERNAME = getenv("BOT_USERNAME", "VahsiMuzikBot")
 PMPERMIT = getenv("PMPERMIT", "DISABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60))
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
