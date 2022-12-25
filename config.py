91190fd485a1aee9581b8dc6309c62289923fb61
mrtxc35 <119897267+mrtxc35@users.noreply.github.com>
2022-12-25 12:56:50 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,14 +6,14 @@ que = {}
 admins = {}
 
 SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "inekMuzik") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "inekMuzikBot")
-PMPERMIT = getenv("PMPERMIT", "ENABLE")
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_NAME = getenv("BOT_NAME", "Vahsi Muzik") 
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+BOT_USERNAME = getenv("BOT_USERNAME", "VahsiMuzikBot")
+PMPERMIT = getenv("PMPERMIT", "DISABLE")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", 60))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
